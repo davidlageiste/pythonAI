4af21b4f6d5b29025cfd8dd25bcc1e1a928e4275
LAGEISTE David <lageiste.david@gmail.com>
2025-01-16 15:14:55 +0100

    fix

--- function_app.py
+++ function_app.py
@@ -8,10 +8,4 @@ app = func.FunctionApp()
 @app.event_grid_trigger(arg_name="event")
 def event_grid_trigger(event: func.EventGridEvent):
     logging.info('Python EventGrid trigger function processed an event')
-    
-    # Get event data
-    event_type = event.event_type
-    echo(event_type)
-    # Call your specific function based on event type or data
-    if event_type == "your.specific.event":
-        pipeline_lyrae_talk()
+    pipeline_lyrae_talk()
